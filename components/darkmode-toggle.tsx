d02b2e28a8f6de3055e39879e8f8e1e674b73b5e
oyesaurabh <oye.saurabhyadav@gmail.com>
2024-10-24 23:12:20 +0530

    minor ui fixes

--- components/darkmode-toggle.tsx
+++ components/darkmode-toggle.tsx
@@ -2,6 +2,7 @@
 import { toast } from "sonner";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -13,24 +14,23 @@ import {
 
 export function DarkmodeToggle() {
   const { setTheme } = useTheme();
-
+  const [darkTheme, setDarkTheme] = useState(true);
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
           variant="outline"
           size="icon"
-          className="w-8 justify-center font-medium text-white border-none hover:bg-white/10 hover:text-white bg-white/40"
+          className="w-8 h-[2rem] justify-center font-medium text-white border-none bg-white/10 hover:text-white hover:bg-white/20"
         >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+          {darkTheme ? <Moon className="" /> : <Sun className="" />}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem
           onClick={() => {
             setTheme("light");
+            setDarkTheme(false);
             toast("Light Theme 🌞");
           }}
         >
@@ -39,18 +39,11 @@ export function DarkmodeToggle() {
         <DropdownMenuItem
           onClick={() => {
             setTheme("dark");
+            setDarkTheme(true);
             toast("Dark Theme 🌚");
           }}
         >
           Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => {
-            setTheme("system");
-            toast("System Theme 💻");
-          }}
-        >
-          System
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
