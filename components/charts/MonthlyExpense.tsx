cbe6b0049da24cdbe224794981f019acc60c66bc
oyesaurabh <oye.saurabhyadav@gmail.com>
2025-02-26 23:33:03 +0530

    feat: enhance MonthlyExpense component with month and category selection for dynamic data fetching

--- components/charts/MonthlyExpense.tsx
+++ components/charts/MonthlyExpense.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
-
 import {
   Card,
   CardContent,
@@ -16,6 +15,20 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
+import { Button } from "../ui/button";
+import { useEffect, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { axiosService } from "@/services";
+import { useCategoryStore } from "@/hooks/useCategoryHook";
+import { Loader2 } from "lucide-react";
+import { CiSearch } from "react-icons/ci";
+
 const chartData = [
   { month: "January", desktop: 186 },
   { month: "February", desktop: 305 },
@@ -32,12 +45,128 @@ const chartConfig = {
   },
 } satisfies ChartConfig;
 
+const monthConfig = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+].map((month) => ({
+  key: month.slice(0, 3).toLowerCase(),
+  value: month,
+}));
+
 export default function MonthlyExpense() {
+  // const [chartType, setChartType] = useState<string>("bar");
+  const [month, setMonth] = useState<string>("jan");
+  const [categoryId, setCategoryId] = useState<string | undefined>(undefined);
+  const [chartData, setChartData] = useState<any[]>([]);
+  const [charDataLoading, setChartDataLoading] = useState(false);
+  const { Categories, isLoading } = useCategoryStore();
+
+  const fetchData = async (payload: any) => {
+    try {
+      setChartDataLoading(true);
+      const { status, data, message } =
+        await axiosService.getTransactionExpenseChart(payload);
+      if (!status) throw new Error(message);
+
+      // prepareChartData(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setChartDataLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isLoading) return;
+    try {
+      const defaultCategoryId = Categories[0]?.id;
+      setCategoryId(defaultCategoryId);
+      const currentMonth = new Date()
+        .toLocaleString("en-US", { month: "short" })
+        .toLowerCase();
+      setMonth(currentMonth);
+      fetchData({ month: currentMonth, categoryId: defaultCategoryId });
+    } catch (error) {
+      console.error("Error setting month:", error);
+    }
+  }, [isLoading]);
+
+  const handleSearch = async () => {
+    try {
+      fetchData({ month, categoryId });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Card>
-      <CardHeader>
-        <CardTitle>Bar Chart</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
+      <CardHeader className="flex justify-between gap-2 space-y-0 border-b py-5 sm:flex-row">
+        <Select value={month} onValueChange={setMonth}>
+          <SelectTrigger
+            className="w-[100px] rounded-lg sm:ml-auto"
+            aria-label="Select a month"
+          >
+            <SelectValue placeholder="Select Month..." />
+          </SelectTrigger>
+          <SelectContent className="rounded-xl">
+            {monthConfig.map((item) => (
+              <SelectItem
+                key={item.key}
+                value={item.key}
+                className="rounded-lg"
+              >
+                {item.value}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        {isLoading ? (
+          <Select>
+            <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
+          </Select>
+        ) : (
+          <Select
+            value={categoryId}
+            onValueChange={setCategoryId}
+            defaultValue={Categories[0]?.id}
+          >
+            <SelectTrigger
+              className="w-[160px] rounded-lg sm:ml-auto"
+              aria-label="Select a value"
+            >
+              <SelectValue placeholder="Select Category..." />
+            </SelectTrigger>
+            <SelectContent className="rounded-xl">
+              {Categories.map((category) => (
+                <SelectItem key={category.id} value={category.id}>
+                  {category.name}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        )}
+        <Button
+          variant={"secondary"}
+          disabled={isLoading || charDataLoading}
+          onClick={handleSearch}
+        >
+          {charDataLoading ? (
+            <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
+          ) : (
+            <CiSearch />
+          )}
+        </Button>
       </CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig}>
--- components/data-charts.tsx
+++ components/data-charts.tsx
@@ -5,9 +5,9 @@ export default function DataCharts() {
   return (
     <div className="flex flex-col gap-4">
       <AreaVariant />
-      <div className="flex flex-row justify-between h-96 gap-2">
-        <MonthlyExpense />
-        <AvgVSCurrent />
+      <div className="flex flex-row justify-between gap-2">
+        {/* <MonthlyExpense /> */}
+        {/* <AvgVSCurrent /> */}
       </div>
     </div>
   );
